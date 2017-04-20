a21096ae1a75ff8c1d08f214f3da59e263ebc5e0
Rick <rickgoldstein12@gmail.com>
2017-04-20 16:29:44 -0400

    reinforce ready for testing

--- Q3/deeprl_hw3/reinforce.py
+++ Q3/deeprl_hw3/reinforce.py
@@ -2,8 +2,13 @@ import gym
 import numpy as np
 import random
 import tensorflow as tf
+import math
 
-def get_total_reward(env, model):
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+
+def get_total_reward(env, model,printTime):
 
 
     minn = 1000000
@@ -22,7 +27,7 @@ def get_total_reward(env, model):
 
         # finish episode
         while (not isTerm):
-            pof1, action = choose_action(model, env.state)
+            pof1, action = choose_action(model, env.env.state)
             newstate, reward, isTerm, junk = env.step(action)
             stepsInEpisode = stepsInEpisode + 1
 
@@ -32,8 +37,10 @@ def get_total_reward(env, model):
             minn = stepsInEpisode
         total = total + stepsInEpisode
 
-    total = total/100.0
 
+    total = total/100.0
+    print printTime,total,minn,maxx
+    
     return total,minn,maxx
 
     """compute total reward
@@ -41,7 +48,7 @@ def get_total_reward(env, model):
     Parameters
     ----------
     env: gym.core.Env
-      The environment.
+      The environment. 
     model: (your action model, which can be anything)
 
     Returns
@@ -49,10 +56,9 @@ def get_total_reward(env, model):
     total_reward: float, min, max
     """
 
-
-
-def choose_action(model, observation):
-    """choose the action
+"""
+def choose_actionDET(model, observation):
+    choose the action 
 
     Parameters
     ----------
@@ -61,7 +67,37 @@ def choose_action(model, observation):
 
     Returns
     -------
-    p: float
+    p: float 
+        probability of action 1
+    action: int
+        the action you choose
+    
+
+    obstemp = np.zeros((1,4))
+    obstemp[0][0] = observation[0]
+    obstemp[0][1] = observation[1]
+    obstemp[0][2] = observation[2]
+    obstemp[0][3] = observation[3]
+
+    # get p1 only from model
+    p1 = model.predict(obstemp)[0][1]
+    if (p1>=.5):
+        return p1,1
+    else:
+        return p1,0
+"""
+
+def choose_action(model, observation):
+    """choose the action 
+
+    Parameters
+    ----------
+    model: (your action model, which can be anything)
+    observation: given observation
+
+    Returns
+    -------
+    p: float 
         probability of action 1
     action: int
         the action you choose
@@ -80,54 +116,113 @@ def choose_action(model, observation):
     else:
         return p1,0
 
+#def test():
+#    env.reset()
+#    obstemp = np.zeros((1,4))
+#    obstemp[0][0] = x[0]
+#    obstemp[0][1] = x[1]
+#    obstemp[0][2] = x[2]
+#    obstemp[0][3] = x[3]
 
+#    obstemp2 = np.zeros((1,4))
+#    obstemp2[0][0] = tf.placeholder(tf.float32,[1])
+#    obstemp2[0][1] = x[1]
+#    obstemp2[0][2] = x[2]
+#    obstemp2[0][3] = x[3]
+
+#    x2 = tf.placeholder(tf.float32, [1, 4])
+#    R = model.predict(x2)
+#    tf.gradients(model.weights,tf.log(R))
+
+    #var = tf.Variable()              # Must be a tf.float32 or tf.float64 variable.
+    #loss = some_function_of(var, data)
+    #data = tf.placeholder(tf.float32,shape = [None,4])
+#    loss = tf.log(model.output)
+#    var_grad = tf.gradients(loss, model.weights)
+    
+#    sess = tf.InteractiveSession()
+    
+#    sess.run(tf.global_variables_initializer())
+#    sess.run(loss,{model.input:[[1,2,3,4]],})
+#    sess.run(var_grad,{model.input:[env.env.state],})
+    
 def reinforce(env):
 
-    MAXITERS = 100000
-    MAXEPISODELENGTH = 100000
+    MAXITERS = 10000
+    #MAXEPISODELENGTH = 1000
     alpha = .1
     gamma = .9
 
     # make policy network (modeL)
-    model = getNetwork()
-    #model.compile(optimizer='rmsprop', loss='mse')
+    model = getNetwork2()
+    #model.compile(optimizer='rmsprop',
+                  #loss='mse')
     # repeat forever
     for i in range(MAXITERS):
         if i % 100 == 0:
-            get_total_reward(env,model)
-
+            get_total_reward(env,model,i)
+        
         stepsInEpisode = 0
+        stateMem = []
 
         # memory to keep track of states
-        stateMemory = np.zeros((MAXEPISODELENGTH,4))
-        savep1 = np.zeros((MAXEPISODELENGTH))
+        #stateMemory = np.zeros((MAXEPISODELENGTH,4))
+        #savep1 = np.zeros((MAXEPISODELENGTH))
         # reset env
         env.reset()
 
         # burnin first step, not in loop
-        stateMemory[0,:] = env.state
-        # env.render()
-        pof1, action = choose_action(model,env.state)
-        savep1[0] = pof1
-        newstate,reward,isTerm, junk = env.step(action)
+        #stateMemory[0,:] = env.env.state
+        stateMem.append(env.env.state)
+        #env.render()
+        pof1, action = choose_action(model,env.env.state)
+        #savep1[0] = pof1
+        newstate,reward,isTerm, junk = env.env.step(action)
         stepsInEpisode = stepsInEpisode+1
 
         #finish episode
-        while(not isTerm and stepsInEpisode < MAXEPISODELENGTH):
-            stateMemory[stepsInEpisode, :] = env.state
-            # env.render()
-            pof1, action = choose_action(model, env.state)
-            savep1[stepsInEpisode] = pof1
+        while(not isTerm): # and stepsInEpisode < MAXEPISODELENGTH):
+            #stateMemory[stepsInEpisode, :] = env.env.state
+            stateMem.append(env.env.state)
+            #env.render()
+            pof1, action = choose_action(model, env.env.state)
+            #savep1[stepsInEpisode] = pof1
             newstate, reward, isTerm, junk = env.step(action)
             stepsInEpisode = stepsInEpisode + 1
 
+        # setup structures for derivs
+        loss = tf.log(model.output)
+        var_grad = tf.gradients(loss, model.weights)
+        sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
+        #sess.run(loss,{model.input:[[1,2,3,4]],})
+        
+        delta = -1
         for j in range(stepsInEpisode):
             # reward is number of steps including and after self (reward is always 1)
             G_t = stepsInEpisode - j
-            a = np.empty((2))
-            a[0] = 1-savep1[j]
-            a[1] = savep1[j]
-            tf.log(a)
+            #a = np.empty((2))
+            #a[0] = 1-savep1[j]
+            #a[1] = savep1[j]
+            #print j
+            #print stateMem
+            #print sess.run(var_grad,{model.input:[stateMem[j]]})
+            gradd = sess.run(var_grad,{model.input:[stateMem[j]]})
+            
+            #get multiplier
+            multt = alpha*math.pow(gamma,j)*G_t
+            #multiply one by one
+            for qq in range(np.size(gradd)):
+                gradd[qq] = gradd[qq]*multt
+                     
+            if delta == -1:
+                delta = gradd
+            else:
+                delta = delta+gradd
+                          
+        desiredWeights = model.get_weights()+ delta           
+        model.set_weights(desiredWeights)
+            #tf.log(a)
             #model = model + alpha*gamma^j*???
 
     """Policy gradient algorithm
@@ -140,10 +235,7 @@ def reinforce(env):
     -------
     total_reward: float
     """
-    return 0
-
-from keras.models import Sequential
-from keras.layers import Dense, Activation
+    return model.get_weights()
 
 
 
@@ -156,14 +248,25 @@ def getNetwork():
     ])
     return model
 
-
+def getNetwork2():
+    model = Sequential([
+        Dense(32, input_shape=(4,)),
+        Activation('relu'),
+        Dense(32),
+        Activation('relu'),
+        Dense(32),
+        Activation('relu'),
+        Dense(2),
+        Activation('softmax'),
+    ])
+    return model
 
 
 def main():
     # my code here
     env = gym.make('CartPole-v0')
 
-    reinforce(env)
+    finalWeights = reinforce(env)
 
     #env.reset()
     #env.render()
